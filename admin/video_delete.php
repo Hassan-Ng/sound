d97a3b1476a0a316c306cd042074216e6d00701a
Hassan-Ng <86090193+Hassan-Ng@users.noreply.github.com>
2025-06-13 12:16:22 +0500

    Deploy

---
+++ admin/video_delete.php
@@ -0,0 +1,24 @@
+<?php
+include("./components/header.php");
+$Id = $_GET['id'];
+
+if ($Id) {
+    $check_sql = "SELECT * FROM songs where id=$Id";
+    $check_result = mysqli_query($conn, $check_sql);
+
+    if (mysqli_num_rows($check_result) > 0) {
+        $sql = "delete from songs where id = $Id";
+        $result = mysqli_query($conn, $sql);
+
+        $fetched_row = mysqli_fetch_assoc($check_result);
+
+        if ($result) {
+            unlink('../videos/' . $fetched_row['song_path']);
+            unlink('../images/' . $fetched_row['song_img']);
+        }
+    }
+}
+
+echo "<script>
+    window.location.href = 'videos_show.php';
+    </script>";
